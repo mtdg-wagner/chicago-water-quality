470f7f8cb10f1f95fd1140ca45dc9a314be1dfb0
Barton Yadlowski <b@bmorphism.us>
2017-04-01 09:54:03 -0400

    Added water icons and color

--- chicago_water_viz/prototype_plots.R
+++ chicago_water_viz/prototype_plots.R
@@ -2,8 +2,15 @@ library(leaflet)
 
 m <- leaflet() %>% setView(lng = -87.5300, lat = 41.8400, zoom = 11)
 
+icons <- awesomeIcons(
+  icon = 'ion-waterdrop',
+  iconColor = 'black',
+  library = 'ion',
+  markerColor = "red"
+)
+
 m %>% addTiles() %>% 
-  addMarkers(-87.6061, 41.8948, popup = "Water for the masses!",
-             label = "Jardine Water Purification Plant") %>%
-  addMarkers(-87.5425, 41.7583, popup = "Was that a solid?",
-             label = "Sawyer Water Purification Plant")
+  addAwesomeMarkers(-87.6061, 41.8948, popup = "Water for the masses!",
+             label = "Jardine Water Purification Plant", icon = icons) %>%
+  addAwesomeMarkers(-87.5425, 41.7583, popup = "Was that a solid?",
+             label = "Sawyer Water Purification Plant", icon = icons)
